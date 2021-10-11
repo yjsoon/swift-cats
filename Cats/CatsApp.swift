d08ed5d5c7f0bcc0b1bc511548a233ce857daca3
YJ Soon <yjsoon@tinkertanker.com>
2021-10-11 12:07:29 +0800

    Initial Commit

---
+++ Cats/CatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsApp.swift
+//  Cats
+//
+//  Created by Yin Jie Soon on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct CatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
